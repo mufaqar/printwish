ea6813ad8c879d3201efe1790f97191f44a16103
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-07-20 20:08:22 +0500

    Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header/Header'
 import { store } from '@/store/store'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
@@ -6,6 +7,7 @@ import { Provider } from 'react-redux'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Header />
       <Component {...pageProps} />
     </Provider>
   )
