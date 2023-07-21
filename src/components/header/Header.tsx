0d4b0013b34394199fb6f5256f23dda0b15cec7a
mufaqar <mufaqar@gmail.com>
2023-07-21 20:21:19 +0500

    Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -204,8 +204,7 @@ const Header = () => {
             </div>
           </div>
         </nav>
-      </header>
-      <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+      </header> 
     </>
   )
 }
