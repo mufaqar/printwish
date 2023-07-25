55763caf5d802f05aa4c6ca134ee6e762e2ddbc9
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-07-25 13:00:28 +0500

    Reviews

---
+++ src/const/feedback.tsx
@@ -0,0 +1,174 @@
+export const Feedback = [
+    {
+        img: "/images/feedback1.webp"
+    },
+    {
+        img: "/images/feedback2.webp"
+    },
+    {
+        img: "/images/feedback3.webp"
+    },
+    {
+        img: "/images/feedback4.webp"
+    },
+    {
+        img: "/images/feedback5.webp"
+    },
+    {
+        img: "/images/feedback6.webp"
+    },
+    {
+        img: "/images/feedback7.webp"
+    },
+    {
+        img: "/images/feedback8.webp"
+    },
+    {
+        img: "/images/feedback9.webp"
+    },
+    {
+        img: "/images/feedback10.webp"
+    },
+    {
+        img: "/images/feedback11.webp"
+    },
+    {
+        img: "/images/feedback12.webp"
+    },
+    {
+        img: "/images/feedback14.webp"
+    },
+    {
+        img: "/images/feedback15.webp"
+    },
+    {
+        img: "/images/feedback16.webp"
+    },
+    {
+        img: "/images/feedback17.webp"
+    },
+    {
+        img: "/images/feedback18.webp"
+    },
+    {
+        img: "/images/feedback19.webp"
+    },
+    {
+        img: "/images/feedback20.webp"
+    },
+    {
+        img: "/images/feedback21.webp"
+    },
+    {
+        img: "/images/feedback22.webp"
+    },
+    {
+        img: "/images/feedback23.webp"
+    },
+    {
+        img: "/images/feedback24.webp"
+    },
+    {
+        img: "/images/feedback25.webp"
+    },
+    {
+        img: "/images/feedback26.webp"
+    },
+    {
+        img: "/images/feedback27.webp"
+    },
+    {
+        img: "/images/feedback28.webp"
+    },
+    {
+        img: "/images/feedback29.webp"
+    },
+    {
+        img: "/images/feedback30.webp"
+    },
+    {
+        img: "/images/feedback31.webp"
+    },
+    {
+        img: "/images/feedback32.webp"
+    },
+    {
+        img: "/images/feedback33.webp"
+    },
+    {
+        img: "/images/feedback34.webp"
+    },
+    {
+        img: "/images/feedback35.webp"
+    },
+    {
+        img: "/images/feedback36.webp"
+    },
+    {
+        img: "/images/feedback37.webp"
+    },
+    {
+        img: "/images/feedback38.webp"
+    },
+    {
+        img: "/images/feedback39.webp"
+    },
+    {
+        img: "/images/feedback40.webp"
+    },
+    {
+        img: "/images/feedback41.webp"
+    },
+    {
+        img: "/images/feedback42.webp"
+    },
+    {
+        img: "/images/feedback43.webp"
+    },
+    {
+        img: "/images/feedback44.webp"
+    },
+    {
+        img: "/images/feedback45.webp"
+    },
+    {
+        img: "/images/feedback46.webp"
+    },
+    {
+        img: "/images/feedback47.webp"
+    },
+    {
+        img: "/images/feedback48.webp"
+    },
+    {
+        img: "/images/feedback49.webp"
+    },
+    {
+        img: "/images/feedback50.webp"
+    },
+    {
+        img: "/images/feedback51.webp"
+    },
+    {
+        img: "/images/feedback52.webp"
+    },
+    {
+        img: "/images/feedback53.webp"
+    },
+    {
+        img: "/images/feedback54.webp"
+    },
+    {
+        img: "/images/feedback55.webp"
+    },
+    {
+        img: "/images/feedback56.webp"
+    },
+    {
+        img: "/images/feedback57.webp"
+    },
+]
+
+export type FeedbackType = {
+    img: any,
+}
