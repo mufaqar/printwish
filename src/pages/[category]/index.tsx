6c5198bcf8a16d39afd4d1ae468068033e6f6f47
syed zunair gillani <gillanix007@gmail.com>
2023-08-02 19:27:48 +0500

    update

--- src/pages/[category]/index.tsx
+++ src/pages/[category]/index.tsx
@@ -5,21 +5,21 @@ import { GetStaticPaths } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-const CategorySlug = ({products}:any) => {
-  const {query} = useRouter()
-  return (
-    <>
-     <PageBanner title={query.category} />
-     <div className='container mx-auto px-3 my-20 w-full'>
-                        <div className='grid sm:grid-cols-4 grid-cols-1 gap-5'>
-                            {products?.map((item:any, idx: number) => {
-                                const img = item?.images[0]?.src
-                                return <Product_Box key={idx} data={item} image={img}/>
-                            })}
-                        </div>
+const CategorySlug = ({ products }: any) => {
+     const { query } = useRouter()
+     return (
+          <>
+               <PageBanner title={query.category} />
+               <div className='container mx-auto px-3 my-20 w-full'>
+                    <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-cols-1 gap-3'>
+                         {products?.map((item: any, idx: number) => {
+                              const img = item?.images[0]?.src
+                              return <Product_Box key={idx} data={item} image={img} />
+                         })}
                     </div>
-    </>
-  )
+               </div>
+          </>
+     )
 }
 
 export default CategorySlug
@@ -28,31 +28,30 @@ export default CategorySlug
 export async function getStaticProps({ params }: any) {
      const slug = params.category
      const dataForCategory = {
-       per_page: 100,
+          per_page: 100,
      };
 
-     const caterogies =  await apiRequest('POST', 'get-products-categories', dataForCategory) 
-     const getID = caterogies.products.find((item:any)=>item.slug === slug)
+     const caterogies = await apiRequest('POST', 'get-products-categories', dataForCategory)
+     const getID = caterogies.products.find((item: any) => item.slug === slug)
 
      const dataForProducts = {
           per_page: 12,
           category: `${getID.id}`,
      }
 
-     const {products} =  await apiRequest('POST', 'get-products', dataForProducts)
+     const { products } = await apiRequest('POST', 'get-products', dataForProducts)
 
      return {
-       props: {
-          products: products,
-       },
+          props: {
+               products: products,
+          },
      };
-   }
-   
-   export const getStaticPaths: GetStaticPaths = async () => {
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
      const paths: any = [];
      return {
-       paths,
-       fallback: 'blocking',
+          paths,
+          fallback: 'blocking',
      };
-   }
-   
+}
