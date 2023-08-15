3e05d4f3a3e873359d4aa6b7119da5f8bd4d4384
syed zunair gillani <gillanix007@gmail.com>
2023-08-15 15:38:47 +0500

    contact us page form

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -6,11 +6,21 @@ function Contact() {
     <main className='items-center mx-auto max-w-screen-xl px-6 md:px-6 py-2.5'>
 <div className="lg:flex justify-between ">
 <div className="txt">
-<p className='text-[#7b858a] text-[20px] font-[400] leading-[27px]'>Contact Form</p>
+<form action="#" className="space-y-3">
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+              <input type="email" id="email" className="shadow-sm lg:w-[700px]  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
+          </div>
+          <div className="sm:col-span-2">
+              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 w-96">Your message</label>
+              <textarea id="message" rows={6} className="block p-2.5 w-full h-16 text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+          </div>
+          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-950 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300  dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+      </form>
 </div>
- <div className="cont mb-[20px]">
-<div className="img cursor-pointer mr-5 ">
-    <img className='w-[477px] h-[128px] ' src="images/Review.png" alt="" />
+ <div className="cont mb-[20px] ">
+<div className="img cursor-pointer lg:mr-5 mt-11">
+    <img className='w-full h-[128px] ' src="images/Review.png" alt="" />
     </div>
     </div>
 </div>
