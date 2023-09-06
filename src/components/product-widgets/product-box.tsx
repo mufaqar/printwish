cceda26b6bc579c51ba2567dad415e672c6055f2
mufaqar <mufaqar@gmail.com>
2023-09-06 23:11:43 +0500

    colors

--- src/components/product-widgets/product-box.tsx
+++ src/components/product-widgets/product-box.tsx
@@ -24,7 +24,7 @@ const Product_Box = ({ data, image }: any) => {
         <Image src={image} alt={data?.title || data?.name} width={200} height={200} className='transition-all duration-200 ease-in-out object-fill w-full mb-3 group-hover:scale-95' />
       </Link>
       <div className='flex flex-col justify-center items-center'>
-        <h5 className="min-h-[52px]">
+        <h5 className="min-h-[40px]">
           <Link href={`/product/${data?.slug}`} className='text-sm font-bold flex font-roboto text-accent hover:text-secondary'>
             <span className='text-center'>{data?.title || data?.name}</span>
           </Link>
@@ -36,14 +36,14 @@ const Product_Box = ({ data, image }: any) => {
         <p className='text-center font-bold font-roboto mb-3 text-primary'>
           From <span className='text-secondary  hover:text-secondary'>£{data?.price}</span>
         </p>
-        <button className='text-base relative font-semibold flex justify-center font-roboto uppercase bg-primary text-white px-6 py-1 hover:bg-transparent hover:text-primary border-primary border-2 hover:border-primary'
-          onClick={() => handleAddToCart(data)}
+        <Link href={`/product/${data?.slug}`}  className='text-base relative font-semibold flex justify-center font-roboto uppercase bg-primary text-white px-6 py-1 hover:bg-transparent hover:text-primary border-primary border-2 hover:border-primary'
+          
         >
-          Add to cart
-          {isProductIndex !== -1 &&
+          View Product
+          {/* {isProductIndex !== -1 &&
             <span className='absolute text-sm bg-red-600 p-1 flex flex-col justify-center items-center w-6 h-6 rounded-full text-white -right-4 -top-3'>{isProductIndex !== -1 ? cartItems[isProductIndex].quantity : ''}</span>
-          }
-        </button>
+          } */}
+        </Link>
       </div>
     </div>
   )
