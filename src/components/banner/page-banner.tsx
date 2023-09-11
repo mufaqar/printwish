d5fca316e16093bd0dbd2c705445b6559addd22e
syed zunair gillani <gillanix007@gmail.com>
2023-09-11 14:12:09 +0500

    page banner

--- src/components/banner/page-banner.tsx
+++ src/components/banner/page-banner.tsx
@@ -4,7 +4,7 @@ const PageBanner = ({ title, category }: any) => {
   const img = category?.image?.src.length > 5 ? category?.image?.src : "/images/merge.png"
   return (
     <section className='p-3 md:p-0'>
-      <div className='py-10 md:py-16 bg-cover bg-no-repeat bg-center' style={{ backgroundImage: `url(${ img })` }}>
+      <div className='py-6 md:py-16 bg-cover bg-no-repeat bg-center' style={{ backgroundImage: `url(${ img })` }}>
         <div className='max-w-screen-xl mx-auto px-4 text-white'>
           <h2 className='sm:text-4xl text-2xl font-semibold font-opensans uppercase mb-5'>
             {title}
