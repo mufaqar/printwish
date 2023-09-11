26a3cb82dfe46a182da153fa491820193dfc3cab
syed zunair gillani <gillanix007@gmail.com>
2023-09-11 12:50:58 +0500

    static link

--- src/components/header/categoriesSubMenu.tsx
+++ src/components/header/categoriesSubMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLinks, NavLinksType } from '@/const/navlinks'
 import Link from 'next/link';
+import { Categories } from '@/const/categories';
 
 const CategoriesSubMenu = ({categories, setMobileMenu}:any) => {
   return (
@@ -9,9 +10,9 @@ const CategoriesSubMenu = ({categories, setMobileMenu}:any) => {
         return (
           <div key={idx}>
             <ul className='md:pb-2'>
-              {categories?.map((sub_item: any, _idx: any) => {
+              {Categories?.map((sub_item: any, _idx: any) => {
                 return <li key={_idx} className="px-5 py-1.5 relative group" onClick={()=>setMobileMenu(false)}>
-                  <Link href={`/${sub_item.slug}`} className="text-xs  cursor-pointer w-full hover:!text-gray-700 !text-white font-semibold text-accent uppercase">
+                  <Link href={`${sub_item.link}`} className="text-xs  cursor-pointer w-full hover:!text-gray-700 !text-white font-semibold text-accent uppercase">
                     {sub_item?.name}
                   </Link>
                   <span className="absolute h-full p-[2px] bg-gray-700 top-0 left-0 hidden group-hover:block"/>
