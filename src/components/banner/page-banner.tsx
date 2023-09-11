b6469f2dc688b26236a61166c64a12316f003565
syed zunair gillani <gillanix007@gmail.com>
2023-09-11 13:25:32 +0500

    update

--- src/components/banner/page-banner.tsx
+++ src/components/banner/page-banner.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const PageBanner = ({title}:any) => {
+const PageBanner = ({ title, category }: any) => {
   return (
-     <section className='py-16 bg-background'>
-     <div className='max-w-screen-xl mx-auto px-4'>
-         <h2 className='sm:text-4xl text-2xl font-semibold font-opensans text-accent uppercase text-center mb-5'>
-         {title}
-         </h2>
-     </div>
- </section>
+    <section className='p-3'>
+      <div className='py-16 bg-background bg-cover bg-no-repeat bg-center' style={{ backgroundImage: `url(${category?.image?.src})` }}>
+        <div className='max-w-screen-xl mx-auto px-4'>
+          <h2 className='sm:text-4xl text-2xl font-semibold font-opensans text-black uppercase mb-5'>
+            {title}
+          </h2>
+          <p>{category?.description}</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/[...page]/index.tsx
+++ src/pages/[...page]/index.tsx
@@ -9,32 +9,33 @@ import { client } from '@/config/client'
 import { LOCATION_PAGE } from '@/config/query'
 import Pagination from '@/components/pagination/pagination'
 
-const CategorySlug = ({ products, slug, pages, productsForLocationPage }: any) => {
+const CategorySlug = ({ products, slug, pages, productsForLocationPage, category }: any) => {
+     console.log("🚀 ~ file: index.tsx:13 ~ CategorySlug ~ products:", category)
      const { query } = useRouter()
      return (
           <>
                {
-                    slug?.includes("t-shirt-printing") ? <Location pages={pages} products={productsForLocationPage}/> : <>
-                         {/* CATEGORY PAGE DATA ↓ */}    
-                         <PageBanner title={query.page} />
+                    slug?.includes("t-shirt-printing") ? <Location pages={pages} products={productsForLocationPage} /> : <>
+                         {/* CATEGORY PAGE DATA ↓ */}
+                         <PageBanner title={query.page} category={category} />
                          {
-                              products.length > 0 ? <div className='container mx-auto px-3 my-20 w-full'>
-                              <div className='grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-cols-2 gap-1 sm:gap-2 md:gap-4'>
-                                   {products?.map((item: any, idx: number) => {
-                                        const img = item?.images[0]?.src
-                                        return <Product_Box key={idx} data={item} image={img} />
-                                   })}
-                              </div>
-                         </div> : <p className='container mx-auto px-3 my-20 w-full text-center'>Result Not Found!</p>
+                              products.length > 0 ? <div className='container mx-auto px-3 my-10 w-full'>
+                                   <div className='grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-cols-2 gap-1 sm:gap-2 md:gap-4'>
+                                        {products?.map((item: any, idx: number) => {
+                                             const img = item?.images[0]?.src
+                                             return <Product_Box key={idx} data={item} image={img} />
+                                        })}
+                                   </div>
+                              </div> : <p className='container mx-auto px-3 my-20 w-full text-center'>Result Not Found!</p>
                          }
-                         
-                    </> 
+
+                    </>
                }
                {/* {
                     products?.length > 12 && <Pagination perPage="3" endpoint={`woocommerce-category-api/v1/product-count/${slug}`}/>
                } */}
-               
-               <div className='mb-16'/>
+
+               <div className='mb-16' />
           </>
 
      )
@@ -46,8 +47,8 @@ export default CategorySlug
 export async function getStaticProps({ params }: any) {
      const p = params.page
      const slug = p[0]
-     
-     if(slug.includes("t-shirt-printing")){
+
+     if (slug.includes("t-shirt-printing")) {
           const response = await client.query({
                query: LOCATION_PAGE,
                variables: {
@@ -66,7 +67,7 @@ export async function getStaticProps({ params }: any) {
           falsy. If it is, it means that the requested location page does not exist or could not be
           found. In this case, the function returns `{ notFound: true }`, which tells Next.js to
           return a 404 page for this specific route. */
-          if(!pages) {
+          if (!pages) {
                return {
                     notFound: true
                }
@@ -80,8 +81,8 @@ export async function getStaticProps({ params }: any) {
                },
           };
      }
-     
-     
+
+
      const dataForCategory = {
           per_page: 100,
      };
@@ -95,7 +96,7 @@ export async function getStaticProps({ params }: any) {
      }
      const { products } = await apiRequest('POST', 'get-products', dataForProducts)
 
-     if(!products) {
+     if (!products) {
           return {
                notFound: true
           }
@@ -104,9 +105,10 @@ export async function getStaticProps({ params }: any) {
           props: {
                products: products,
                slug,
+               category: getID
           },
      };
-     
+
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -115,5 +117,5 @@ export const getStaticPaths: GetStaticPaths = async () => {
           paths,
           fallback: 'blocking',
      };
-     
+
 }
