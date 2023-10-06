d0d34cb58c13254355ba69cb6cb1e7a9e3fcaac9
syed zunair gillani <zunairgillani54@gmail.com>
2023-10-06 15:47:42 +0500

    update

--- src/pages/success.jsx
+++ src/pages/success.jsx
@@ -2,9 +2,10 @@
 import { clearAll } from '@/features/AddToCart';
 import useOrderHandler from '@/hooks/useOrderHandler';
 import { useRouter } from 'next/router';
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { orderData } from '../../public/data';
+import Link from 'next/link';
 
 const Success = () => {
   const dispatch = useDispatch();
@@ -24,9 +25,9 @@ const Success = () => {
       billing.first_name = formdata.name
       billing.phone = formdata.mobile
       billing.address_1 = formdata.address
-      
+
       var cartItems = JSON.parse(sessionStorage.getItem('cartItems'));
-     
+
       OrderSubmit(cartItems)
       dispatch(clearAll())
       sessionStorage.removeItem('cartItems')
@@ -37,7 +38,25 @@ const Success = () => {
 
   return (
     <React.StrictMode>
-      <div>Success</div>      
+      <div class="h-[calc(100vh-130px)]  flex flex-col justify-center items-center">
+        <div class="bg-white p-6  md:mx-auto">
+          <svg viewBox="0 0 24 24" class="text-green-600 w-16 h-16 mx-auto my-6">
+            <path fill="currentColor"
+              d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z">
+            </path>
+          </svg>
+          <div class="text-center">
+            <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">You've successfully placed the order!</h3>
+            <p class="text-gray-600 my-2">Thank you for completing your secure online payment. We will contact you soon</p>
+            <p> Have a great day!  </p>
+            <div class="py-10 text-center">
+              <Link href="/" class="px-12 capitalize bg-secondary hover:bg-primary text-white font-semibold py-3">
+                  back to home
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.StrictMode>
   );
 };
