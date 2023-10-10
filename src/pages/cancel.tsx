9c78d2d2a41de8a6346dde2c1d8fa06cb8fb4621
syed zunair gillani <zunairgillani54@gmail.com>
2023-10-10 18:40:26 +0500

    cancel page

--- src/pages/cancel.tsx
+++ src/pages/cancel.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { TbBasketOff } from 'react-icons/tb'
 
 const Cancel = () => {
   return (
-    <div>Cancel</div>
+    <div className='flex flex-col items-center text-center my-20'>
+      <TbBasketOff size={80} color="red"/>
+      Your order are canceled!
+    </div>
   )
 }
 
