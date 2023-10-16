c2091c5102e6d63f3ce1270cdfeef781e633cfd0
mufaqar <mufaqar@gmail.com>
2023-10-16 18:52:57 +0500

    Chat

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -120,107 +120,107 @@ export default function Delivery_information() {
                         <div className="hea">
                             <h2 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Understanding Cookies</h2>
                             <p className='text-[#1e2d35] pb-[1.25rem]'>A cookie is a tiny text file that's dispatched to your computer or mobile device (henceforth mentioned as a “device”) by a web server. Its purpose is to let the website remember certain details about your online activity on that site. Cookies gather details about your website interactions, specifics about your device like its IP address and the browser being used, demographic information, and, if you landed on our platform via a link from another website, that website’s URL. For users who are registered or subscribed, cookies might also capture your name and email, which can then be shared with data processors for verification.</p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>Cookies enable a more tailored online experience by noting your preferences. The insights they provide allow us to refine how you engage with our platform, ultimately enhancing your overall experience.</p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>Depending on their longevity, cookies can be:</p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>Cookies enable a more tailored online experience by noting your preferences. The insights they provide allow us to refine how you engage with our platform, ultimately enhancing your overall experience.</p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>Depending on their longevity, cookies can be:</p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 <strong>Session Cookies: </strong> These are temporary and only stay on your device while you're browsing. They're removed once you shut down your browser.</p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'> <strong> Persistent Cookies: </strong>These cookies remain on your device for a set period after you've closed your browser and are reactivated each time you revisit the originating website.
-
+                            <p className='text-[#1e2d35] pb-[1.25rem]'> 
+                            <strong> Persistent Cookies: </strong>These cookies remain on your device for a set period after you've closed your browser and are reactivated each time you revisit the originating website.
                             </p>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Different Cookie Categories</h3>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Different Cookie Categories</h3>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Essential Cookies</h3>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>These are fundamental for our platform's operation. Without them, certain functionalities would be inoperable. They don’t trace your online journey elsewhere and steer clear of collecting marketing-oriented data.
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Essential Cookies</h3>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>These are fundamental for our platform's operation. Without them, certain functionalities would be inoperable. They don’t trace your online journey elsewhere and steer clear of collecting marketing-oriented data.
                                 Functional Cookies </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
 
                                 Their purpose is to recollect your settings on our platform and deliver an enhanced, personalized user experience. The data they assemble is typically anonymous, meaning it won’t identify you. While these cookies don’t monitor your broader internet activities or gather ad-selling data, they do play a role in ad presentation.
                             </p>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Analytical Performance Cookies</h3>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Analytical Performance Cookies</h3>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 These cookies keep track of our platform's efficacy, like tallying page views or determining unique visitor counts. Some might be set up and managed by external parties. They offer insights into user interaction trends, allowing us to refine the user experience or spotlight parts of the website needing improvement. Importantly, this data is impersonal (i.e., it doesn’t hold personal identifiers like names or email addresses) and is leveraged solely for statistical evaluation.
                             </p>
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>MARKETING COOKIES POLICY</h3>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>MARKETING COOKIES POLICY</h3>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Understanding Marketing Cookies</h3>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Understanding Marketing Cookies</h3>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Marketing cookies, either set by us or our vetted external partners, take note of your website visits. They utilize this data to offer you advertisements curated to match your interests. This method, frequently termed as online behavioural advertising (OBA), segments interests based on your online browsing patterns. Insights from your browsing can infer specific attributes about you (like age, gender, etc.), refining the relevance of ads across the web. Though these cookies track online behavior, they don’t personally identify you, even if logged into our site. Without them, the ads you stumble upon online may not align with your preferences. To get more insights on OBA and opting out of these cookies, please explore: www.youronlinechoices.com.
 
                             </p>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>  Our Application of Cookies</h3>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>  Our Application of Cookies</h3>
 
                             In association with our reputable partners, we employ cookies for several reasons:
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Essential and Functional Cookies</h3>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Essential and Functional Cookies</h3>
 
                             Recognize repeat visitors and present them with a site customized to their liking.
                             Forego the need for regulars to input login data repeatedly.
                             Facilitate commentary on our platforms.
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Analytical Performance Cookies</h3>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Analytical Performance Cookies</h3>
 
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>  Through tools like Google Analytics and comScore Digital Analytix, we gauge user interactions. This helps us discern which content resonates most, aiding in enhancing our offerings and reporting our accomplishments.
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>  Through tools like Google Analytics and comScore Digital Analytix, we gauge user interactions. This helps us discern which content resonates most, aiding in enhancing our offerings and reporting our accomplishments.
                                 Marketing Cookies</p>
 
-                             <p className='text-[#1e2d35] pb-[1.25rem]'> Assess universal user behavior on our and partner sites to draft a profile. This ensures advertising is aptly directed based on user inclinations. For instance, if a user explores a camera review, the cookie captures this, potentially resulting in targeted advertising related to that camera.
+                            <p className='text-[#1e2d35] pb-[1.25rem]'> Assess universal user behavior on our and partner sites to draft a profile. This ensures advertising is aptly directed based on user inclinations. For instance, if a user explores a camera review, the cookie captures this, potentially resulting in targeted advertising related to that camera.
                             </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Compile profiles that reliable third parties can leverage to better focus their ads.</p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
 
 
                                 Our emails might include web beacons (clear GIFs or web bugs) to monitor our marketing endeavors' efficacy. If you access an email from us, we can trace your subsequent interactions on our site.
                             </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 The data gathered by the cookies concerning your site interactions (inclusive of your IP address) might be sent to and stored on servers located in the U.S. or U.K. This data may also be shared with third parties under legal obligations or if they process data on their behalf. Engaging with this site means you acquiesce to the data processing executed by these service providers, as detailed above.
                             </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Note: We don’t have oversight or access capabilities to cookies deployed on your device by third-party advertisers or sponsors.
                             </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 OTHER THIRD-PARTY COOKIES EXPLAINED
                             </p>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Third-Party Cookies on Our Platforms</h3>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Third-Party Cookies on Our Platforms</h3>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 While browsing our website, you might discover cookies not directly associated with us. If a page embeds content from external platforms like YouTube or Facebook, they may place their unique cookies on your device. We don't dictate or access these cookies since they are retrievable only by their originator. For detailed information, refer to the third-party websites.
                             </p>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Printwish UK’s Cookie Oversight</h3>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>  How do we monitor cookies on our platforms, and how can you manage or opt out? </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Printwish UK’s Cookie Oversight</h3>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>  How do we monitor cookies on our platforms, and how can you manage or opt out? </p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 The concept of websites storing data on users' devices can be unsettling for some, especially when third parties utilize this information. For instance, receiving ads tailored to your interests based on your online history might not be welcome. If you choose to opt out of cookies:
                             </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Existing cookies can be eradicated from your device.
                             </p>   <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Modify your browser settings to manage cookies. Some settings enable primary site cookies while blocking third-party cookies. Others restrict cookies from specific advertisers or erase all cookies. However, doing so may limit site functionality. For more guidance, visit www.allaboutcookies.org or your browser's help section.
                             </p>
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Opt-out Options</h3>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Opt-out Options</h3>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Analytical Performance Cookies: </h3>To withdraw from Analytics cookies, use the following:
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Analytical Performance Cookies: </h3>To withdraw from Analytics cookies, use the following:
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Google Analytics: Google Analytics Opt-out </p>  <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Behavioural Advertising Cookies: To disable cookies from advertisers or ad service providers:
                             </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 Google Ads Preferences</p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'> Audience Science Consumer Options    </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>  Admeld Privacy Policy    </p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>  For other online behavioural ad opt-outs, refer to:    </p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'> Audience Science Consumer Options    </p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>  Admeld Privacy Policy    </p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>  For other online behavioural ad opt-outs, refer to:    </p>
 
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>  Network Advertising Initiative Opt-out Page</p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>  Network Advertising Initiative Opt-out Page</p>
 
-                            <h3  className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Additional Information and Contact</h3>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'>
+                            <h3 className='font-[400] text-[30px] text-[#1d2127] pb-[32px]'>Additional Information and Contact</h3>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'>
                                 For inquiries or more on our cookies, reach out to Printwish UK’s Data Protection team:</p>
 
 
-                             <p className='text-[#1e2d35] pb-[1.25rem]'> Email: enquiries@printwish.co.uk</p>
-                             <p className='text-[#1e2d35] pb-[1.25rem]'> Phone: 0800-051-0821</p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'> Email: enquiries@printwish.co.uk</p>
+                            <p className='text-[#1e2d35] pb-[1.25rem]'> Phone: 0800-051-0821</p>
 
 
 
