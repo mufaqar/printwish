252926f7bd2d02a0ab6f97a7a59bb2fbe945c99e
mufaqar <mufaqar@gmail.com>
2023-10-23 14:16:51 +0500

    reverse

--- src/const/feedback.tsx
+++ src/const/feedback.tsx
@@ -1,52 +1,52 @@
 export const Feedback = [
     {
-        img: "/images/reviews/review-1.jpg"
-    },
-    {
-        img: "/images/reviews/review-2.jpg"
-    },
-    {
-        img: "/images/reviews/review-3.jpg"
-    },
-    {
-        img: "/images/reviews/review-4.jpg"
-    },
-    {
-        img: "/images/reviews/review-5.jpg"
-    },
-    {
-        img: "/images/reviews/review-6.jpg"
-    },
-    {
-        img: "/images/reviews/review-7.jpg"
-    },
-    {
-        img: "/images/reviews/review-8.jpg"
-    },
-    {
-        img: "/images/reviews/review-9.jpg"
-    },
-    {
-        img: "/images/reviews/review-10.jpg"
-    },
-    {
-        img: "/images/reviews/review-11.jpg"
-    },
-    {
-        img: "/images/reviews/review-12.jpg"
-    },
-    {
-        img: "/images/reviews/review-13.jpg"
-    },
-    {
-        img: "/images/reviews/review-14.jpg"
+        img: "/images/reviews/review-16.jpg"
     },
     {
         img: "/images/reviews/review-15.jpg"
     },
     {
-        img: "/images/reviews/review-16.jpg"
+        img: "/images/reviews/review-14.jpg"
     },
+    {
+        img: "/images/reviews/review-13.jpg"
+    },
+    {
+        img: "/images/reviews/review-12.jpg"
+    },
+    {
+        img: "/images/reviews/review-11.jpg"
+    },
+    {
+        img: "/images/reviews/review-10.jpg"
+    },
+    {
+        img: "/images/reviews/review-9.jpg"
+    },
+    {
+        img: "/images/reviews/review-8.jpg"
+    },
+    {
+        img: "/images/reviews/review-7.jpg"
+    },
+    {
+        img: "/images/reviews/review-6.jpg"
+    },
+    {
+        img: "/images/reviews/review-5.jpg"
+    },
+    {
+        img: "/images/reviews/review-4.jpg"
+    },
+    {
+        img: "/images/reviews/review-3.jpg"
+    },
+    {
+        img: "/images/reviews/review-2.jpg"
+    },
+    {
+        img: "/images/reviews/review-1.jpg"
+    }
 ]
 
 export type FeedbackType = {
