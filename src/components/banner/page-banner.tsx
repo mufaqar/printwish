9d7582cbd7499be836bacbe6bc5fec98666cc34d
mufaqar <mufaqar@gmail.com>
2023-10-31 19:55:37 +0500

    opactiy

--- src/components/banner/page-banner.tsx
+++ src/components/banner/page-banner.tsx
@@ -6,7 +6,7 @@ const PageBanner = ({ title, category, slug }: any) => {
   const img = category?.image?.src.length > 5 ? category?.image?.src : "/images/merge.png"
   return (
     <section>
-      <div className='py-6 relative md:py-16 bg-cover bg-no-repeat bg-center' style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)), url(${img})` }}>
+      <div className='py-6 relative md:py-16 bg-cover bg-no-repeat bg-center' style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 36,63,0.9), rgba(0, 36,63,0.9)), url(${img})` }}>
         <div className='container mx-auto px-4  text-white'>
           <h2 className='sm:text-4xl text-2xl font-bold font-opensans capitalize mb-2'>
             {title}
