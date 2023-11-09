7da8b2de8c6a4b57587b538359cab0570d828038
syed zunair gillani <zunairgillani54@gmail.com>
2023-11-09 16:35:50 +0500

    layout change on mobile

--- src/components/customiztionProduct/CustomiztionProduct.tsx
+++ src/components/customiztionProduct/CustomiztionProduct.tsx
@@ -5,9 +5,9 @@ import Slider from "react-slick";
 import { SettingsContext } from '@/context/global-context';
 
 
-const CustomiztionProduct = ({number}:any) => {
+const CustomiztionProduct = ({ number }: any) => {
 
-     const { selectedCustomizedLayout, setSelectedCustomizedLayout, selectedProduct, setDesignPosition} = useContext(SettingsContext)
+     const { selectedCustomizedLayout, setSelectedCustomizedLayout, selectedProduct, setDesignPosition } = useContext(SettingsContext)
 
      const settings = {
           dots: false,
@@ -19,48 +19,48 @@ const CustomiztionProduct = ({number}:any) => {
                {
                     breakpoint: 1200,
                     settings: {
-                      slidesToShow: 4,
-                      slidesToScroll: 1,
+                         slidesToShow: 4,
+                         slidesToScroll: 1,
                     }
                },
                {
-                 breakpoint: 1024,
-                 settings: {
-                   slidesToShow: 3,
-                   slidesToScroll:1,
-                 }
+                    breakpoint: 1024,
+                    settings: {
+                         slidesToShow: 3,
+                         slidesToScroll: 1,
+                    }
                },
                {
-                 breakpoint: 600,
-                 settings: {
-                   slidesToShow: 3,
-                   slidesToScroll: 1,
-                   initialSlide: 1
-                 }
+                    breakpoint: 600,
+                    settings: {
+                         slidesToShow: 3,
+                         slidesToScroll: 1,
+                         initialSlide: 1
+                    }
                },
                {
-                 breakpoint: 480,
-                 settings: {
-                   slidesToShow: 2,
-                   slidesToScroll: 1
-                 }
+                    breakpoint: 480,
+                    settings: {
+                         slidesToShow: 2,
+                         slidesToScroll: 1
+                    }
                }
-             ]
+          ]
      };
 
-     const handleDesignPosition = ((designPosition:any) => {
+     const handleDesignPosition = ((designPosition: any) => {
           setDesignPosition(designPosition)
           setSelectedCustomizedLayout(designPosition);
           // setSelectedProduct({...selectedProduct, designPosition})
-          
+
      })
 
      return (
           <>
-               <section className='mt-4 bg-background p-8 rounded-lg'>
-                    <h5 className='text-xl font-semibold text-accent font-roboto'>Step 2 - Select Positions:</h5>
-                    <p>You can select one position at one time. To add another customisation, complete this step and click on Add Customisation {number}.</p>
-                    <div className='mt-5'>
+               <section className='mt-4 bg-background p-5 md:p-8 rounded-lg'>
+                    <h5 className='text-xl font-semibold text-accent pl-2 font-roboto'>Step 2 - Select Positions:</h5>
+                    <p className='pl-2'>You can select one position at one time. To add another customisation, complete this step and click on Add Customisation {number}.</p>
+                    <div className='mt-5 hidden md:block'>
                          <Slider {...settings}>
                               {designVarient.map((item, idx) => {
                                    return (
@@ -74,6 +74,19 @@ const CustomiztionProduct = ({number}:any) => {
                                    )
                               })}
                          </Slider>
+                    </div>
+                    <div className='md:hidden grid grid-cols-2 mt-2'>
+                         {designVarient.map((item, idx) => {
+                              return (
+                                   <button className='p-1' key={idx} onClick={() => handleDesignPosition(item.position)}>
+                                        <div className={`bg-white flex flex-col items-center gap-2 border-[3px] rounded-lg p-1 py-2 ${selectedCustomizedLayout === item.position ? 'border-secondary' : 'border-transparent'}`}>
+                                             <h5 className="uppercase font-light font-roboto text-center">{item?.placement}</h5>
+                                             <Image src={item.image} alt={item.position} width={400} height={400} className='w-20' />
+                                             <h5 className="capitalize font-light font-roboto  text-center">{item.position}</h5>
+                                        </div>
+                                   </button>
+                              )
+                         })}
                     </div>
                </section>
 
