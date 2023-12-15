73b96e8d8624b6d24bb9519af78e1929aafec2a5
mufaqar <mufaqar@gmail.com>
2023-12-15 17:06:24 +0500

    update optimization

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["printwish.co.uk", "backend.printwish.co.uk", "res.cloudinary.com"],
-    unoptimized: true
   },
   // experimental: {
   //   esmExternals: 'loose'
   // }
+  //  unoptimized: true
 }
 
 module.exports = nextConfig
