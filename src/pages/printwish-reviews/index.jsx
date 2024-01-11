f382eb97e2593a2bb3c67a8b7441f5451367ec7d
mufaqar <mufaqar@gmail.com>
2024-01-11 18:22:20 +0500

    PRINTWISH

--- src/pages/printwish-reviews/index.jsx
+++ src/pages/printwish-reviews/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Reviews from '../../components/reviews/reviews'
+import SeoMeta from '@/components/seo/Seo'
 
 const PrintwishReview = () => {
   return (
 
     <>
     <SeoMeta title="PRINTWISH Reviews | Printwish" description="Looking for a Bulk T shirt printing service in London, UK? get custom t shirts at wholesale price? We can guarantee cheap prices on bulk orders." url="printwish-reviews" />
-
-  
     <section className='container mx-auto px-3 py-10'>
       <div className='p-4 text-center font-bold text-gray-600 text-2xl border mb-5'>PRINTWISH Reviews </div>
       <Reviews/>
