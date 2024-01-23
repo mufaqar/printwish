266de1fa05dc519504134eadbbb16ed756dbca54
mufaqar <mufaqar@gmail.com>
2024-01-23 15:09:33 +0500

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  trailingSlash: true,
+ // trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: [
@@ -10,31 +10,31 @@ const nextConfig = {
       'res.cloudinary.com',
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/wholesale-bag',
-        destination: '/cotton-bags-cheap-branded-tote-bags',
-        permanent: true,
-      },
-      {
-        source: '/cotton-bags',
-        destination: '/cotton-bags-cheap-branded-tote-bags',
-        permanent: true,
-      },
-      {
-        source: '/t-shirt-printing-kent/',
-        destination: '/custom-t-shirt-printing-cheap-t-shirt-printing',
-        permanent: true,
-      },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/wholesale-bag',
+  //       destination: '/cotton-bags-cheap-branded-tote-bags',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/cotton-bags',
+  //       destination: '/cotton-bags-cheap-branded-tote-bags',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/t-shirt-printing-kent/',
+  //       destination: '/custom-t-shirt-printing-cheap-t-shirt-printing',
+  //       permanent: true,
+  //     },
 
-      {
-        source: '/t-shirts/',
-        destination: '/custom-t-shirt-printing-cheap-t-shirt-printing',
-        permanent: true,
-      },
-    ];
-  },
+  //     {
+  //       source: '/t-shirts/',
+  //       destination: '/custom-t-shirt-printing-cheap-t-shirt-printing',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
   // experimental: {
   //   esmExternals: 'loose'
   // }
